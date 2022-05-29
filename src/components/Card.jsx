c4bc668fbb15c64729720c6c5c18d0a638feef11
socarrandinn <socarrandin@gmail.com>
2022-05-29 19:32:23 -0400

    commit final

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { restFecha } from '../services/restFecha'
 export const Card = (props) => {
   const { title, description, fechaCreacion, fechaPublicacion, presupuesto, categoria } = props
   const compactPresupuesto = compactNumber(presupuesto)
-  
+
   return (
     <div className="h-auto w-full relative rounded-md shadow-lg shadow-slate-200 bg-white px-3 pt-8 pb-3 mt-2 flex flex-col gap-y-1">
       {props.urgente === true && <span className="text-xs bg-amarillo-pollito mr-auto absolute top-0 left-3 font-light text-white p-1 " >Se precisa urgente</span>}
@@ -12,9 +12,13 @@ export const Card = (props) => {
       <p className="text-xs font-normal overflow-hidden overflow-ellipsis break-all">{description}</p>
 
       <label className="text-[0.5em] xl:text-xs  flex flex-row gap-1 pt-2 items-center mr-auto">
-      <span className="overflow-ellipsis overflow-hidden whitespace-nowrap" >CUP {compactPresupuesto}</span> •
-      <span className="overflow-ellipsis overflow-hidden">{categoria}</span> •
-      <span className="overflow-ellipsis overflow-hidden">{restFecha(fechaCreacion, fechaPublicacion)} días</span>
+        <ol className="text-xs xl:text-xs flex gap-x-2 items-center justify-center">
+          <li className="text-slate-500">CUP {compactPresupuesto}</li>
+          <li className="bg-azul-oscuro h-0.5 w-0.5 rounded-full"></li>
+          <li className="text-slate-500">{categoria}</li>
+          <li className="bg-azul-oscuro h-0.5 w-0.5 rounded-full"></li>
+          <li className='text-slate-500'>{restFecha(fechaCreacion, fechaPublicacion)}</li>
+        </ol>        
       </label>
 
     </div>
